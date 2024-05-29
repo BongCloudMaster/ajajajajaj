c8dc30446f75a89d85e839e0e2b01eca3ca790e7
BongCloudMaster <126247984+BongCloudMaster@users.noreply.github.com>
2024-05-29 21:16:08 +0800

    Update and rename inventoryUILoader to inventoryUILoader.lua

--- inventoryUILoader.lua
+++ inventoryUILoader.lua
@@ -1,9 +1,12 @@
 if not inventory_ui_ops then
+    print('no ui ops load default one instead.')
     loadstring(game:HttpGet('https://raw.githubusercontent.com/BongCloudMaster/ajajajajaj/main/inventoryUI.lua'))()
 else
     if inventory_ui_ops.Viewport then
+         print('viewport is true load one with viewport.')
         loadstring(game:HttpGet('https://raw.githubusercontent.com/BongCloudMaster/ajajajajaj/main/inventoryUIWithViewport.lua'))()
     else
+         print('viewport is false load default one instead.')
         loadstring(game:HttpGet('https://raw.githubusercontent.com/BongCloudMaster/ajajajajaj/main/inventoryUI.lua'))()
     end
 end
